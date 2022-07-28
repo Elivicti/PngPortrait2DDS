b885873be0c1476789243aa4dda1b1ad0c3a67a2
Elivicti <edwynb72@gmail.com>
2022-07-28 14:21:26 +0800

    添加中文本地化

--- PngPortrait2DDS/main.cpp
+++ PngPortrait2DDS/main.cpp
@@ -1,10 +1,21 @@
 #include "PngPortrait2DDS.h"
 #include <QtWidgets/QApplication>
+#include <QTranslator>
+
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
-    PngPortrait2DDS w;
-    w.show();
-    return a.exec();
+	QApplication app(argc, argv);
+	QTranslator* translator = new QTranslator(&app);
+
+	if (QLocale().language() == QLocale::Chinese)
+	{
+		bool success = translator->load(":/Translation/zh_cn.qm");
+		if (success)
+			app.installTranslator(translator);
+	}
+
+	PngPortrait2DDS w;
+	w.show();
+	return app.exec();
 }
---
+++ PngPortrait2DDS/zh_cn.ts
@@ -0,0 +1,190 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_CN">
+<context>
+    <name>PngPortrait2DDS</name>
+    <message>
+        <location filename="PngPortrait2DDS.cpp" line="80"/>
+        <source>Select Preset</source>
+        <translation>选择预设</translation>
+    </message>
+    <message>
+        <location filename="PngPortrait2DDS.cpp" line="80"/>
+        <location filename="PngPortrait2DDS.cpp" line="490"/>
+        <source>Json File(*.json)</source>
+        <translation>Json文件(*.json)</translation>
+    </message>
+    <message>
+        <location filename="PngPortrait2DDS.cpp" line="101"/>
+        <source>Select Portrait Directory</source>
+        <translation>选择立绘文件夹</translation>
+    </message>
+    <message>
+        <location filename="PngPortrait2DDS.cpp" line="119"/>
+        <source>Loading Completed: %1 Total</source>
+        <translation>加载完成：共计%1项</translation>
+    </message>
+    <message>
+        <location filename="PngPortrait2DDS.cpp" line="282"/>
+        <location filename="PngPortrait2DDS.cpp" line="486"/>
+        <location filename="PngPortrait2DDS.cpp" line="650"/>
+        <source>Error</source>
+        <translation>错误</translation>
+    </message>
+    <message>
+        <location filename="PngPortrait2DDS.cpp" line="283"/>
+        <source>The width and height of the image must be a multiple of 4.</source>
+        <translation>图像的宽与高必须为4的整数倍。</translation>
+    </message>
+    <message>
+        <location filename="PngPortrait2DDS.cpp" line="284"/>
+        <location filename="PngPortrait2DDS.cpp" line="486"/>
+        <location filename="PngPortrait2DDS.cpp" line="650"/>
+        <source>OK</source>
+        <translation>确定</translation>
+    </message>
+    <message>
+        <location filename="PngPortrait2DDS.cpp" line="358"/>
+        <source>Processing (%1/%2)...</source>
+        <translation>处理中 (%1/%2)…</translation>
+    </message>
+    <message>
+        <source>Processing...</source>
+        <translation type="vanished">处理中……</translation>
+    </message>
+    <message>
+        <source>Cancel</source>
+        <translation type="vanished">取消</translation>
+    </message>
+    <message>
+        <source>Canceled.</source>
+        <translation type="vanished">已取消。</translation>
+    </message>
+    <message>
+        <location filename="PngPortrait2DDS.cpp" line="471"/>
+        <source>Completed.</source>
+        <translation>已完成。</translation>
+    </message>
+    <message>
+        <location filename="PngPortrait2DDS.cpp" line="486"/>
+        <source>You must load any data before save.</source>
+        <translation>必须先加载有数据才能保存。</translation>
+    </message>
+    <message>
+        <location filename="PngPortrait2DDS.cpp" line="490"/>
+        <source>Save Preset</source>
+        <translation>保存预设</translation>
+    </message>
+    <message>
+        <location filename="PngPortrait2DDS.cpp" line="650"/>
+        <source>Invalid Json File.</source>
+        <translation>无效的Json文件。</translation>
+    </message>
+</context>
+<context>
+    <name>PngPortrait2DDSClass</name>
+    <message>
+        <location filename="PngPortrait2DDS.ui" line="63"/>
+        <source>Browse</source>
+        <translation>浏览</translation>
+    </message>
+    <message>
+        <location filename="PngPortrait2DDS.ui" line="108"/>
+        <source>Preview</source>
+        <translation>预览</translation>
+    </message>
+    <message>
+        <location filename="PngPortrait2DDS.ui" line="127"/>
+        <source>Image Size</source>
+        <translation>图像大小</translation>
+    </message>
+    <message>
+        <location filename="PngPortrait2DDS.ui" line="138"/>
+        <source>Width</source>
+        <translation>宽</translation>
+    </message>
+    <message>
+        <location filename="PngPortrait2DDS.ui" line="162"/>
+        <source>Height</source>
+        <translation>高</translation>
+    </message>
+    <message>
+        <location filename="PngPortrait2DDS.ui" line="184"/>
+        <source>Use Seperate Settings</source>
+        <translation>使用独立设定</translation>
+    </message>
+    <message>
+        <location filename="PngPortrait2DDS.ui" line="191"/>
+        <source>Portrait Offset</source>
+        <translation>立绘偏移</translation>
+    </message>
+    <message>
+        <location filename="PngPortrait2DDS.ui" line="248"/>
+        <source>Portrait Scale</source>
+        <translation>立绘缩放</translation>
+    </message>
+    <message>
+        <location filename="PngPortrait2DDS.ui" line="298"/>
+        <source>Export Options</source>
+        <translation>导出选项</translation>
+    </message>
+    <message>
+        <location filename="PngPortrait2DDS.ui" line="314"/>
+        <source>Portrait Registration</source>
+        <translation>肖像注册</translation>
+    </message>
+    <message>
+        <location filename="PngPortrait2DDS.ui" line="321"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Determines if the program will generate an effect file and a yml file which will allow you to link portrait to its proper name.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;勾选后将会生成可对应立绘与名称的Effect和yml文件。&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+    <message>
+        <location filename="PngPortrait2DDS.ui" line="324"/>
+        <source>Proper Name Effect</source>
+        <translation>立绘对应正确名称</translation>
+    </message>
+    <message>
+        <location filename="PngPortrait2DDS.ui" line="334"/>
+        <source>Export</source>
+        <translation>导出</translation>
+    </message>
+    <message>
+        <location filename="PngPortrait2DDS.ui" line="353"/>
+        <source>Options</source>
+        <translation>选项</translation>
+    </message>
+    <message>
+        <location filename="PngPortrait2DDS.ui" line="367"/>
+        <source>Load Preset</source>
+        <translation>加载预设</translation>
+    </message>
+    <message>
+        <location filename="PngPortrait2DDS.ui" line="372"/>
+        <source>Save Preset</source>
+        <translation>保存预设</translation>
+    </message>
+</context>
+<context>
+    <name>PortraitTable</name>
+    <message>
+        <location filename="PortraitTable.cpp" line="38"/>
+        <source>Portrait</source>
+        <translation>立绘文件</translation>
+    </message>
+    <message>
+        <location filename="PortraitTable.cpp" line="38"/>
+        <source>Species</source>
+        <translation>物种</translation>
+    </message>
+    <message>
+        <location filename="PortraitTable.cpp" line="38"/>
+        <source>leader</source>
+        <translation>领袖</translation>
+    </message>
+    <message>
+        <location filename="PortraitTable.cpp" line="38"/>
+        <source>Ruler</source>
+        <translation>统治者</translation>
+    </message>
+</context>
+</TS>
